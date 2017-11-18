156a4b02074b5945897d2aafd93b1e0c09a8b701
Peter Muys <peter@petermuys.com>
2017-11-18 12:30:46 +0100

    persistent-remoter added

---
+++ persistent-libs/persistent-remoter/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * TODOC
+ *
+ * @author petermuys
+ * @since 18/11/17
+ */
+module persistent.remoter {
+	requires transitive persistent.result;
+	requires transitive persistent.collections;
+}
