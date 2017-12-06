b1804c033a3476856841507b0f077828879b870b
Peter Muys <peter.muys@kbc.be>
2017-12-06 09:54:38 +0100

    pom for build @ kbc

--- persistent-libs/persistent-sql/persistent-sql-connect/src/main/java/module-info.java
+++ persistent-libs/persistent-sql/persistent-sql-connect/src/main/java/module-info.java
@@ -4,8 +4,9 @@
  * @since 19/11/17
  */
 module persistent.sql.connect {
-	requires transitive persistent.core;
 	requires transitive persistent.result;
+	requires transitive persistent.log;
 	requires java.sql;
+	requires persistent.doc.annotations;
 	exports com.persistentbit.sql.connect;
 }
